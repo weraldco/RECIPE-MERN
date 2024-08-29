f7bfbc78bea302bf2dcc6cbbcfc3b2cfdca22aaa
Werald Opolento <werald.opolento@gmail.com>
2024-08-30 07:40:45 +0800

    Working on the server side, authentication

---
+++ SERVER/routes/users.js
@@ -0,0 +1,55 @@
+import bcrypt from 'bcrypt';
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import { UserModel } from '../model/User.js';
+
+const router = express.Router();
+
+router.post('/register', async (req, res) => {
+	const { username, password } = req.body;
+
+	const user = await UserModel.findOne({ username: username }).exec();
+	if (user)
+		return res.status(400).json({ message: 'your username already taken.' });
+
+	try {
+		const hashPassword = await bcrypt.hash(password, 10);
+
+		console.log(`${username} - ${hashPassword}`);
+		const result = await UserModel.create({
+			username: username,
+			password: hashPassword,
+		});
+
+		res.json(result);
+	} catch (err) {
+		console.error(err);
+	}
+});
+
+router.post('/login', async (req, res) => {
+	const { username, password } = req.body;
+
+	if (!username)
+		return res.status(200).json({ message: 'You need to a username' });
+	if (!password)
+		return res.status(200).json({ message: 'You need to a password' });
+	try {
+		const foundUser = await UserModel.findOne({ username: username }).exec();
+		if (!foundUser)
+			return res
+				.status(401)
+				.json({ message: 'User not found. Register first to login.' });
+
+		const isMatch = await bcrypt.compare(password, foundUser.password);
+
+		if (!isMatch) return res.status(400).json({ message: 'Wrong password' });
+
+		res
+			.status(201)
+			.json({ message: `You successfully login your account.${username}` });
+	} catch (err) {
+		res.status(401).json({ message: err.message });
+	}
+});
+export { router as userRouter };
--- SERVER/src/index.js
+++ SERVER/src/index.js
@@ -3,11 +3,13 @@ import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 import { connectDB } from '../config/dbConnection.js';
+import { userRouter } from '../routes/users.js';
 dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use('/auth', userRouter);
 connectDB();
 
 app.listen(3001, () => console.log('Server is running in port: 3001'));
