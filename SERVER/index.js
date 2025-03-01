f94c7776e0b9d0413f26a191e4fcbc29bd4c81c4
Werald Opolento <werald.opolento@gmail.com>
2025-03-02 07:21:23 +0800

    fix the folders

--- SERVER/index.js
+++ SERVER/index.js
@@ -3,22 +3,20 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
-import { connectDB } from '../config/dbConnection.js';
-import { categoryRouter } from '../routes/category.js';
-import { recipeRouter } from '../routes/recipe.js';
-import { userRouter } from '../routes/users.js';
+// import { connectDB } from '../config/dbConnection.js';
+import { connectDB } from './config/dbConnection.js';
+import { categoryRouter } from './routes/category.js';
+import { recipeRouter } from './routes/recipe.js';
+import { userRouter } from './routes/users.js';
 
 dotenv.config();
 const app = express();
 
 connectDB();
 app.use(express.json());
-app.use(
-	cors({
-		origin: 'https://spoonful-recipes.vercel.app',
-	})
-);
+app.use(cors({}));
 app.use('/auth', userRouter);
 app.use('/recipes', recipeRouter);
 app.use('/category', categoryRouter);
 app.listen(3001, () => console.log('Server is running in port: 3001'));
+// test
